76bd164b01f7507ab7c8fa9f3a1750681c959ae0
pramod <pramod.nandy06@gmail.com>
2015-10-04 12:13:45 +0530

    Update ReleasePdfParser.py

--- ReleasePdfParser.py
+++ ReleasePdfParser.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import csv
+import re
 from binascii import b2a_hex
 
 
@@ -13,10 +14,11 @@ from pdfminer.pdfdocument import PDFDocument, PDFNoOutlines
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import PDFPageAggregator
-from pdfminer.layout import LAParams, LTTextBox, LTTextLine, LTFigure, LTImage, LTChar
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine, LTChar
 
 outputCSVFile = "Final.txt"
 inputPdfFile = "xxxx"
+csvFileDelimiter = "#"
 
 def openPdf (pdf_doc, fn, pwd, *args):
     result = None
@@ -38,16 +40,14 @@ def openPdf (pdf_doc, fn, pwd, *args):
         fp.close()
     except IOError:
         # the file doesn't exist or similar problem
-	#print("Error in File opening")
      	result = "Error"
-     	#pass
     return result
 
 
 ###
 ### Extracting Text
 ###
-import re
+
 
 def convertString (s, enc='utf-8'):
     if s:
@@ -68,7 +68,7 @@ def parsePdfText (h, lt_obj, pct=0.2):
             hash_x1 = k[1]
             if x1 >= (hash_x1 * (1.0-pct)) and (hash_x1 * (1.0+pct)) >= x1:
                 # the text inside this LT* object was positioned at the same
-                # width as a prior series of text, so it belongs together
+                # width as a prior series OutputFILE text, so it belongs together
                 key_found = True
 		if not (convertString(lt_obj.get_text())) == ' \n':
 			new_str = convertString(lt_obj.get_text()).strip(' \n\t\r')
@@ -84,49 +84,48 @@ def parsePdfText (h, lt_obj, pct=0.2):
 
 
 def parsePdfObjects (lt_objs, page_number, text=[]):
-    page_text = {} # k=(x0, x1) of the bbox, v=list of text strings within that bbox width (physical column)
-    for lt_obj in lt_objs:
-        if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
-            # text, so arrange is logically based on its column width
-            page_text = parsePdfText(page_text, lt_obj)
+	page_text = {} 
+    	for lt_obj in lt_objs:
+		#For Text
+        	if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+            		page_text = parsePdfText(page_text, lt_obj)
 
-	#For Image/Picturs     
-        #elif isinstance(lt_obj, LTImage):
+		#For Image/Picturs     
+        	#elif isinstance(lt_obj, LTImage):
     
-    of = open(outputCSVFile,"w")
-    ow = csv.writer(of,delimiter='#',lineterminator='\n\n')
-    for k, v in sorted([(key,value) for (key,value) in page_text.items()]):
-        # sort the page_text hash by the keys (x0,x1 values of the bbox),
-        # which produces a top-down, left-to-right sequence of related columns
-
-	lst=[]
-	#Remove New Line Entry From CSV file Entry
-	if v[0] != ' \n':
-		for i in v:
-			lst.append(i.strip(' \n'))
-		ow.writerow(lst)
-    of.close()
+    	OutputFILE = open(outputCSVFile,"w")
+    	outputFileWrite = csv.writer(OutputFILE,delimiter=csvFileDelimiter,lineterminator='\n\n')
+    	for k, v in sorted([(key,value) for (key,value) in page_text.items()]):
+		lst=[]
+		#Remove New Line Entry From CSV file Entry
+		if v[0] != ' \n':
+			for i in v:
+				lst.append(i.strip(' \n'))
+			outputFileWrite.writerow(lst)
+   	OutputFILE.close()
 
 
 ###
 ### Processing Pages
 ###
+def getPdfInterpret():
+	rsrcmgr = PDFResourceManager()
+    	laparams = LAParams()
+    	device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+    	interptr = PDFPageInterpreter(rsrcmgr, device)
+	return interptr
 
 def parsePdf (doc):
-    rsrcmgr = PDFResourceManager()
-    laparams = LAParams()
-    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-
-    for i, page in enumerate(PDFPage.create_pages(doc)):
-	    interpreter.process_page(page)
-       	    # receive the LTPage object for this page
-       	    layout = device.get_result()
-       	    # layout is an LTPage object which may contain child objects like LTTextBox, LTFigure, LTImage, etc.
-	    parsePdfObjects(layout, (i+1))
+	interptr = getPdfInterpret() 
+	for i, page in enumerate(PDFPage.create_pages(doc)):
+		interptr.process_page(page)
+       	    	# receive the LTPage object for this page
+       	    	layout = device.get_result()
+       	    	# layout is an LTPage object which may contain child objects like LTTextBox, LTFigure, LTImage, etc.
+	    	parsePdfObjects(layout, (i+1))
 
 def main (pdf_doc, pwd=''):
-    return openPdf(pdf_doc, parsePdf, pwd)
+	return openPdf(pdf_doc, parsePdf, pwd)
 
 
 if __name__=="__main__":
